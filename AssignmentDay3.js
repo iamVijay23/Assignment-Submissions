ef28101b4d44ac9efc463ff20905d2cb32fe2b64
VijaySingh Maurya <52886042+iamVijay23@users.noreply.github.com>
2022-01-27 19:47:48 +0530

    Add files via upload
    AssignmentDay3   Submitted !!

---
+++ AssignmentDay3.js
@@ -0,0 +1,11 @@
+const email = prompt('Please enter your email');
+const password = prompt('Please enter your password');
+
+if(email== "stu@letsupgrad.com" && password== "12345")
+{
+    alert('You are logged in successfully');
+}
+else
+{
+    alert("Try again with correct email & password");
+}
